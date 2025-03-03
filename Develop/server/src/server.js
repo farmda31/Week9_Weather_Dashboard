497e900bcb18a1b79bf85ca402019e67edff2061
farmda31 <daniel.farmer3@gmail.com>
2025-03-02 18:17:17 -0600

    Add files via upload

---
+++ Develop/server/src/server.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import express from 'express';
+//import path from 'path';
+dotenv.config();
+// Import the routes
+import routes from './routes/index.js';
+const app = express();
+const PORT = process.env.PORT || 3001;
+// TODO: Serve static files of entire client dist folder
+app.use(express.json()); // This is the middle ware for parsing JSON
+// TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: true })); // Middle ware for parsing URL-encoded data
+// TODO: Implement middleware to connect the routes
+app.use(routes);
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
